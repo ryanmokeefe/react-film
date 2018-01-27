fc0681e7fc1837968b0bce4f6da4f53c24f9b0c6
Ryan OKeefe <ryanokeefe208@gmail.com>
2018-01-27 13:25:35 -0500

    extracted year

--- src/FilmRow.js
+++ src/FilmRow.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 
 class FilmRow extends Component {
     render() {
+        let year = (new Date(this.props.film.release_date)).getFullYear()
 
         return (
             <div>
                 <img src={"https://image.tmdb.org/t/p/w780" + this.props.film.poster_path} alt="poster" />
                 <div className="film-summary">
                     <h1>{this.props.film.title}</h1>
-                    <p>{this.props.film.year}</p>
+                    <p>{year}</p>
                 </div>
             </div>
         )
